7239d8d772eb92e3bd67b4b619d898181589e7fc
Uaná Gasparim <uanabgasparim@gmail.com>
2023-09-26 10:56:11 -0300

    text field component done

---
+++ website/website_module/components/text_field.svelte
@@ -0,0 +1,21 @@
+<script lang=ts>
+    export let hint:string;
+    
+</script>
+
+<style lang=scss>
+    input[type=text], select {
+    width: 100%;
+    height: 100%;
+    padding: 12px 20px;
+    margin: 8px 0;
+    display: inline-block;
+    border: 6px solid #9CE685;
+    border-radius: 60px;
+    box-sizing: border-box;
+    }
+</style>
+
+<div>
+    <input type="text" id="text_field" name="firstname" placeholder={hint}>
+</div>
---
+++ website/website_module/src/App.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import TextField from './../components/text_field.svelte';
+</script>
+
+<main>
+  <div>
+    <TextField hint="usuário"/>
+    <TextField hint="senha"/>
+  </div>
+</main>
+
+<style>
+  
+</style>
